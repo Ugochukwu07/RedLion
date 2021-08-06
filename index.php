068165013efb01df91ce4bab01115ff4c93e71f2
Ugochukwu07 <ekwuemeugochukwu83@gmail.com>
2021-08-06 19:50:21 +0100

    all 3

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $title = 'Home';
 <!DOCTYPE html>
 <html lang="en">
 
-<?php include(ROOT_PATH . '/APP/INCLUDES/links_open_top.php'); ?>
+<?php include(ROOT_PATH . '/app/includes/links_open_top.php'); ?>
 
 <body class="st-container st-effect-3">
     <div class="wrapper">
--- path.php
+++ path.php
@@ -1,9 +1,32 @@
 <?php 
-/* error_reporting(0); 
-define('', '');*/
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on'){  
+    $url = "https://";   
+}else{ 
+    $url = "http://"; 
+}  
+// Append the host(domain name, ip) to the URL.   
+$url.= $_SERVER['HTTP_HOST'];
+$LOCAL = strpos($url, 'localhost:8080') ? 1 : 0;
 
-define('ROOT_PATH', realpath(dirname(__FILE__)));
-define('BASE_URL', 'http://localhost:8080/sync');
+if($LOCAL){
+    define('ROOT_PATH', realpath(dirname(__FILE__)));
+    define('BASE_URL', 'http://localhost:8080/RedLion');
+    define('HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'mazzy_dap');
+    define("DEMO", true);
+}else{
+    define('ROOT_PATH', realpath(dirname(__FILE__)));
+    error_reporting(0);
+    define('BASE_URL', 'https://storelad.com/RedLion');
+    define('HOST', 'localhost');
+    define('DB_USER', 'mazzydap_admin');
+    define('DB_PASS', 'omo]2R1+4mK6SC');
+    define('DB_NAME', 'mazzydap_main');
+    define("DEMO", false);
+}
+//echo ROOT_PATH;
 
 include('define.php');
 //helpers function
